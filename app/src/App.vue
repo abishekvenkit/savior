05375e64cdc77d11fe07b3fe654aa30b0d47caf9
Stephen P <stephen.palathingal@gmail.com>
2019-01-10 12:44:30 -0500

    basic login page

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!--<router-link to="/">Home</router-link> |-->
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
     <router-view/>
   </div>
 </template>

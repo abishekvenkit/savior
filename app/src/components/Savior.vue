56d5f869cf58a027c2ad34e87b9fb566739975f5
Ruben Krueger <ruben1@stanford.edu>
2019-01-10 11:44:56 -0500

    cleaned

---
+++ app/src/components/Savior.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h1>Savior</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Savior',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+</style>

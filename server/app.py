1c51dc123d0d5f4995ef07aaa06ce5880c0cdef0
Abishek Venkit <abishekvenkit@gmail.com>
2019-01-11 01:01:20 -0500

    finished most of backend

--- server/app.py
+++ server/app.py
@@ -21,11 +21,6 @@ apiKey = 'fa0bca11d2dce6398f771fd7ed49ba42'
 cred = credentials.Certificate("service.json")
 firebase_admin.initialize_app(cred)
 
-# grouponMap = {'Food & Groceries': ['food-and-drink'] ,
-# 'Fun': ['entertainment-and-media', 'beauty-and-spas'],
-#  'Clothing': ['retail', 'mens-clothing-shoes-and-accessories', 'womens-clothing-shoes-and-accessories'], 
-# 'Essentials': ,
-# , 'Electronics': ['electronics'], 'Health':, 'Other':,}
 
 #create routes with respective template files
 
@@ -85,10 +80,10 @@ def purchases():
 			decodedLatLng[geohash2.decode(code)] = 1
 
 	out = sorted(decodedLatLng, reverse=True, key=decodedLatLng.get)
-	out = out[0:5]
+	locations = out[0:5]
 
-	parseGroupon(locations)
-	return out
+	outputData = parseGroupon(locations)
+	return (str(outputData))
 	#return json.dumps(purchaseData)
 
 def parsePurchase(purchase):
@@ -109,11 +104,81 @@ def parseMerchant(merchantId):
 	 "lng": merchantLatLng["lng"], "category": merchantCategory}
 	return merchantInfo
 
-def parseGroupon(locations):
-	for loc in locations:
-		
-	return
+def retrieveGroupon(jsonData):
+	output = []
+	#print (str(jsonData))
+	for i in range(5):
+		out = {}
+		try:
+			out['title'] = jsonData['deals'][i]['title']
+		except:
+			return None
+		rawUrl = jsonData['deals'][i]['dealUrl']
+		intermediate = rawUrl[109:]
+		out['url'] = 'https://www.groupon.com/deals/' + intermediate[:-123]
+		out['image'] = jsonData['deals'][i]['grid4ImageUrl']
+		out['discountP'] = jsonData['deals'][i]['options'][0]['discountPercent']
+		output.append(out)
+	return output
 
+def parseGroupon(locations):
+	outputData = {'electronicsData':[], 'foodAndDrinkData':[],
+	'funData': [], 'clothingData':[], 'essentialsData':[],
+	'healthData':[], 'otherData': []}
+
+	loc = locations[0]
+	#Electronics Data
+	url = 'https://partner-api.groupon.com/deals.json?tsToken=UK_AFF_0_211827_212556_0&lat={}&lng={}&radius:20&filters=category:electronics&offset=0&limit=5'.format(loc[0], loc[1])
+	r = requests.get(url = url)
+	# extracting data in json format
+	electronicsData = r.json()
+	#print (electronicsData['deals'])
+	#print (str(electronicsData))
+	outputData['electronicsData'].append(retrieveGroupon(electronicsData))
+
+	#Food and Drink
+	url = 'https://partner-api.groupon.com/deals.json?tsToken=UK_AFF_0_211827_212556_0&lat={}&lng={}&radius:20&filters=category:food-and-drink&offset=0&limit=5'.format(loc[0], loc[1])
+	r = requests.get(url = url)
+	# extracting data in json format
+	foodAndDrinkData = r.json()
+	outputData['foodAndDrinkData'].append(retrieveGroupon(foodAndDrinkData))
+
+	#Fun
+	url = 'https://partner-api.groupon.com/deals.json?tsToken=UK_AFF_0_211827_212556_0&lat={}&lng={}&radius:20&filters=category:entertainment-and-media&filters=category:beauty-and-spas&offset=0&limit=5'.format(loc[0], loc[1])
+	r = requests.get(url = url)
+	# extracting data in json format
+	funData = r.json()
+	outputData['funData'].append(retrieveGroupon(funData))
+
+	#Clothing
+	url = 'https://partner-api.groupon.com/deals.json?tsToken=UK_AFF_0_211827_212556_0&lat={}&lng={}&radius:20&filters=category:retail&filters=category:mens-clothing-shoes-and-accessories&filters=category:womens-clothing-shoes-and-accessories&offset=0&limit=5'.format(loc[0], loc[1])
+	r = requests.get(url = url)
+	# extracting data in json format
+	clothingData = r.json()
+	outputData['clothingData'].append(retrieveGroupon(clothingData))
+
+	#Essentials
+	url = 'https://partner-api.groupon.com/deals.json?tsToken=UK_AFF_0_211827_212556_0&lat={}&lng={}&radius:20&filters=category:for-the-home&filters=category:groceries-household-and-pets&offset=0&limit=5'.format(loc[0], loc[1])
+	r = requests.get(url = url)
+	# extracting data in json format
+	essentialsData = r.json()
+	outputData['essentialsData'].append(retrieveGroupon(essentialsData))
+
+	#Health
+	url = 'https://partner-api.groupon.com/deals.json?tsToken=UK_AFF_0_211827_212556_0&lat={}&lng={}&radius:20&filters=category:health-and-fitness&filters=category:health-and-beauty&offset=0&limit=5'.format(loc[0], loc[1])
+	r = requests.get(url = url)
+	# extracting data in json format
+	healthData = r.json()		
+	outputData['healthData'].append(retrieveGroupon(healthData))
+
+	#Other
+	url = 'https://partner-api.groupon.com/deals.json?tsToken=UK_AFF_0_211827_212556_0&lat={}&lng={}&radius:20&filters=category:automotive&filters=category:tour-travel&offset=0&limit=5'.format(loc[0], loc[1])
+	r = requests.get(url = url)
+	# extracting data in json format
+	otherData = r.json()
+	outputData['otherData'].append(retrieveGroupon(otherData))
+
+	return outputData
 
 
 if __name__ == "__main__":
